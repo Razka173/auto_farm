ca334e1811b242621f6396fe153c5ef434357017
Razka Agniatara <razka173@gmail.com>
2026-02-16 22:06:33 +0700

    refactor: rename bot to main

--- main.py
+++ main.py
@@ -9,16 +9,7 @@ from dotenv import load_dotenv
 # Load environment variables from .env file
 load_dotenv()
 
-trophiebypass = True
 attacktype = "TapTap"
-
-gold_target = 250000
-elixir_target = 250000
-dark_elixir_target = 500
-
-gold_remaining = 50000
-elixir_remaining = 50000
-dark_elixir_remaining = 50
 
 # Fetch port from environment variable or use default from attacks.py
 p  = os.getenv("BOT_PORT", '5555')  # Default to the value from attacks.py if BOT_PORT is not set
@@ -28,7 +19,7 @@ bot_running = threading.Event()
 def start_bot():
     global bot_thread
     if not bot_running.is_set():
-        bot_thread = threading.Thread(target=Bot, daemon=True)
+        bot_thread = threading.Thread(target=loop, daemon=True)
         bot_thread.start()
         app.log("Bot started.")
 
@@ -36,7 +27,7 @@ def stop_bot():
     bot_running.clear()
     app.log("Stopping bot...")
 
-def Bot():
+def loop():
     bot_running.set()
 
     while bot_running.is_set():
@@ -62,72 +53,104 @@ def Bot():
             "spell_slots": spell_slots,
         }
 
+        # do detect state
+        f.check_current_state(p)
+        if f.check_current_state.is_lost_connection:
+            f.tap(465, 524, p)
+        if f.check_current_state.is_another_connect:
+            f.tap(458, 519, p)
+        if f.check_current_state.is_star_bonus:
+            f.tap(814, 768, p)
+
         app.log("Searching for base...")
         f.find(p)
         t.sleep(6)
         f.check_loot(p)
 
+        allow_attack = False
+        finish_attack = False
+
+        cond1 = cond2 = cond3 = True
+        if (gold_target_enabled):
+            cond1 = int(f.check_loot.result[0]) > gold_target
+        if (elixir_target_enabled):
+            cond2 = int(f.check_loot.result[1]) > elixir_target
+        if (dark_elixir_target_enabled):
+            cond3 = int(f.check_loot.result[2]) > dark_elixir_target
+
         # check loot state
-        while (
-                (
-                    int(f.check_loot.result[0]) < gold_target and gold_target_enabled == True
-                )
-                or (
-                    int(f.check_loot.result[1]) < elixir_target and elixir_target_enabled == True
-                )
-                or (
-                    int(f.check_loot.result[2]) < dark_elixir_target and dark_elixir_target_enabled == True
-                )
-            ) and bot_running.is_set():
-            app.log("base found: " +  format(int(f.check_loot.result[0]), ",") + " Gold   " + format(int(f.check_loot.result[1]), ",") + " Elixir   " +  format(int(f.check_loot.result[2]), ",") + " Dark Elixir")
+        while ((not cond1 or not cond2 or not cond3) and bot_running.is_set()):
+            # do detect state
+            f.check_current_state(p)
+            if f.check_current_state.is_lost_connection:
+                f.tap(465, 524, p)
+            if f.check_current_state.is_another_connect:
+                f.tap(458, 519, p)
+            if f.check_current_state.is_star_bonus:
+                f.tap(814, 768, p)
+
+            app.log("base found: " +  format(int(f.check_loot.result[0]), ",") + " Gold   " \
+                + format(int(f.check_loot.result[1]), ",") + " Elixir   " \
+                +  format(int(f.check_loot.result[2]), ",") + " Dark Elixir"
+            )
             app.log("loot below target, going to next base")
             f.next(p)
             t.sleep(6)
             f.check_loot(p) # recursive loot check
 
+            cond1 = cond2 = cond3 = True
+            if (gold_target_enabled):
+                cond1 = int(f.check_loot.result[0]) > gold_target
+            if (elixir_target_enabled):
+                cond2 = int(f.check_loot.result[1]) > elixir_target
+            if (dark_elixir_target_enabled):
+                cond3 = int(f.check_loot.result[2]) > dark_elixir_target
+
+        allow_attack = True
         # attack state
-        if bot_running.is_set():
-            app.log("base found: " +  format(int(f.check_loot.result[0]), ",") + " Gold   " + format(int(f.check_loot.result[1]), ",") + " Elixir   " +  format(int(f.check_loot.result[2]), ",") + " Dark Elixir")
-            app.log("going to attack")
+        if bot_running.is_set() and allow_attack:
+            app.log("base found: " +  format(int(f.check_loot.result[0]), ",") + " Gold   " \
+                + format(int(f.check_loot.result[1]), ",") + " Elixir   " \
+                +  format(int(f.check_loot.result[2]), ",") + " Dark Elixir"
+            )
+            app.log("attacking...")
             do_attack = getattr(a, attacktype)
             do_attack(params=attack_params)
             app.log("attack deployed, waiting for battle to finish")
             t.sleep(9)
-        else:
-            return
+            finish_attack = True
+
         
-        # check loot during attack
-        if bot_running.is_set():
-            app.log("checking loot during attack")
-            f.check_loot(p)
-            recurring = 0
-            while (
-                int(f.check_loot.result[0]) > gold_remaining and 
-                int(f.check_loot.result[1]) > elixir_remaining and 
-                int(f.check_loot.result[2]) > dark_elixir_remaining
-            ) and bot_running.is_set():
-                # if loot is still above remaining threshold, continue attack
-                t.sleep(2)
-                f.check_loot(p)
-                recurring += 1
-                if recurring >= 3:
-                    return_home = f.check_return_home(p)
-                    app.log("return home check: " + str(return_home))
-                    if return_home == True:
-                        break
+        # # check loot during attack
+        # if bot_running.is_set():
+        #     app.log("checking loot during attack")
+        #     f.check_loot(p)
+        #     recurring = 0
+        #     while (
+        #         int(f.check_loot.result[0]) > gold_remaining and 
+        #         int(f.check_loot.result[1]) > elixir_remaining and 
+        #         int(f.check_loot.result[2]) > dark_elixir_remaining
+        #     ) and bot_running.is_set():
+        #         # if loot is still above remaining threshold, continue attack
+        #         t.sleep(2)
+        #         f.check_loot(p)
+        #         recurring += 1
+        #         if recurring >= 3:
+        #             return_home = f.check_return_home(p)
+        #             app.log("return home check: " + str(return_home))
+        #             if return_home == True:
+        #                 break
 
         app.log("stopping attack, returning to base")
-        if bot_running.is_set():
+        if bot_running.is_set() and finish_attack:
             app.log("returning")
             t.sleep(2)
             f.tap(800,770,p)
             t.sleep(5)
-        else:
-            return
 
 #################################################################################################### 
 # Create main window
-class ClashOfClansBotApp:
+class ClasherControls:
     def __init__(self):
         self.App = ctk.CTk()
         self.App.title("Otomatisasi Robot CoC")
@@ -205,20 +228,28 @@ class ClashOfClansBotApp:
         self.dark_elixir_remaining_entry.grid(row=2, column=4, padx=10, pady=5)  # Increased padding
 
         # Troop and Heroes Slots
+        # troop
         ctk.CTkLabel(self.App.target_frame, text="Troop Slot(s):").grid(row=3, column=0, padx=10, pady=5, sticky="w")  # Increased padding
         self.troop_slots_entry = ctk.CTkEntry(self.App.target_frame)
-        self.troop_slots_entry.insert(1, "1")  # Default value
+        self.troop_slots_entry.insert(0, "1")  # Default value
         self.troop_slots_entry.grid(row=3, column=1, padx=10, pady=5)  # Increased padding
 
-        ctk.CTkLabel(self.App.target_frame, text="Heroes Slot(s):").grid(row=4, column=0, padx=10, pady=5, sticky="w")  # Increased padding
-        self.heroes_slots_entry = ctk.CTkEntry(self.App.target_frame)
-        self.heroes_slots_entry.insert(1, "1")  # Default value
-        self.heroes_slots_entry.grid(row=4, column=1, padx=10, pady=5)  # Increased padding
+        ctk.CTkLabel(self.App.target_frame, text="Workshop Slot(s):").grid(row=4, column=0, padx=10, pady=5, sticky="w")  # Increased padding
+        self.workshop_slots_entry = ctk.CTkEntry(self.App.target_frame)
+        self.workshop_slots_entry.insert(0, "1")  # Default value
+        self.workshop_slots_entry.grid(row=4, column=1, padx=10, pady=5)  # Increased padding
 
-        ctk.CTkLabel(self.App.target_frame, text="Spell Slot(s):").grid(row=5, column=0, padx=10, pady=5, sticky="w")  # Increased padding
+        # heroes
+        ctk.CTkLabel(self.App.target_frame, text="Heroes Slot(s):").grid(row=5, column=0, padx=10, pady=5, sticky="w")  # Increased padding
+        self.heroes_slots_entry = ctk.CTkEntry(self.App.target_frame)
+        self.heroes_slots_entry.insert(0, "1")  # Default value
+        self.heroes_slots_entry.grid(row=5, column=1, padx=10, pady=5)  # Increased padding
+
+        # spell
+        ctk.CTkLabel(self.App.target_frame, text="Spell Slot(s):").grid(row=6, column=0, padx=10, pady=5, sticky="w")  # Increased padding
         self.spell_slots_entry = ctk.CTkEntry(self.App.target_frame)
-        self.spell_slots_entry.insert(1, "1")  # Default value
-        self.spell_slots_entry.grid(row=5, column=1, padx=10, pady=5)  # Increased padding
+        self.spell_slots_entry.insert(0, "1")  # Default value
+        self.spell_slots_entry.grid(row=6, column=1, padx=10, pady=5)  # Increased padding
 
     def toggle_gold_target(self):
         if self.gold_target_check.get():
@@ -238,6 +269,12 @@ class ClashOfClansBotApp:
         else:
             self.dark_elixir_target_entry.configure(state="disabled")
 
+    def safe_int(self, entry, default=0):
+        try:
+            return int(entry.get())
+        except ValueError:
+            return default
+
     def log(self, message):
         textbox = self.App.log_textbox._textbox
         textbox.tag_configure("spacing", spacing3=8)
@@ -246,24 +283,25 @@ class ClashOfClansBotApp:
 
     def get_targets(self):
         return {
-            "gold_target": int(self.gold_target_entry.get()),
-            "elixir_target": int(self.elixir_target_entry.get()),
-            "dark_elixir_target": int(self.dark_elixir_target_entry.get()),
-            "gold_remaining": int(self.gold_remaining_entry.get()),
-            "elixir_remaining": int(self.elixir_remaining_entry.get()),
-            "dark_elixir_remaining": int(self.dark_elixir_remaining_entry.get()),
+            "gold_target": self.safe_int(self.gold_target_entry),
+            "elixir_target": self.safe_int(self.elixir_target_entry),
+            "dark_elixir_target": self.safe_int(self.dark_elixir_target_entry),
+            "gold_remaining": self.safe_int(self.gold_remaining_entry),
+            "elixir_remaining": self.safe_int(self.elixir_remaining_entry),
+            "dark_elixir_remaining": self.safe_int(self.dark_elixir_remaining_entry),
             "gold_target_enabled": self.gold_target_check.get(),
             "elixir_target_enabled": self.elixir_target_check.get(),
             "dark_elixir_target_enabled": self.dark_elixir_target_check.get(),
-            "troop_slots": int(self.troop_slots_entry.get()),
-            "heroes_slots": int(self.heroes_slots_entry.get()),
-            "spell_slots": int(self.spell_slots_entry.get()),
+            "troop_slots": self.safe_int(self.troop_slots_entry),
+            "workshop_slots": self.safe_int(self.workshop_slots_entry),
+            "heroes_slots": self.safe_int(self.heroes_slots_entry),
+            "spell_slots": self.safe_int(self.spell_slots_entry),
         }
 
     def run(self):
         self.App.mainloop()
 
-cls_clash_of_clans_bot_app = ClashOfClansBotApp()
+cls_clash_of_clans_bot_app = ClasherControls()
 
 # Instantiate and run the application
 if __name__ == "__main__":
